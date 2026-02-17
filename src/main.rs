2d88a39b44e5142cc612cde327d7c3cd360f3a6b
Artsiom Dzenisiuk <artsiom.d@icloud.com>
2026-02-17 20:44:08 +0100

    Added prototype tokenizer

---
+++ src/main.rs
@@ -0,0 +1,61 @@
+use std::fs;
+use std::fmt;
+use clap::Parser;
+
+/// Zea compiler
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Args {
+    #[arg(short, long)]
+    filename: String,
+}
+
+enum Token {
+    Plus,
+    Minus,
+    Times,
+    Divide,
+    Number(u64),
+}
+
+fn tokenize(input: &String) -> Vec<Token> {
+    let mut tokens: Vec<Token> = vec![];
+    for c in input.chars() {
+        if c.is_whitespace() {
+            continue
+        }
+
+        match c {
+            '+' => tokens.push(Token::Plus),
+            '-' => tokens.push(Token::Minus),
+            '*' => tokens.push(Token::Times),
+            '/' => tokens.push(Token::Divide),
+            _ => todo!()
+        }
+    }
+    tokens
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Token::Plus => write!(f, "+"),
+            Token::Minus => write!(f, "-"),
+            Token::Times => write!(f, "*"),
+            Token::Divide => write!(f, "/"),
+            Token::Number(x) => write!(f, "{x}"),
+        }
+    }
+}
+
+fn main() -> Result<(), std::io::Error> {
+    let args = Args::parse();
+    let contents = fs::read_to_string(args.filename)?;  
+    let tokens = tokenize(&contents);
+
+    for token in tokens {
+        println!("{token}");
+    }
+    
+    Ok(())
+}
