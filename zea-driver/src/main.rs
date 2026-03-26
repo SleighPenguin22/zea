cbd4f0e116c663d0429638da6c527a636faa8565
Iwan Snel <sneliwan40@gmail.com>
2026-03-26 13:27:37 +0100

    bare driver for parsing and desugaring test file

--- zea-driver/src/main.rs
+++ zea-driver/src/main.rs
@@ -5,20 +5,31 @@
 //     pub filename: String,
 // }
 
+use zea_ast::PrettyAST;
+use std::fs::read_to_string;
+use std::process::exit;
+use zea_ast::zea::NodeExpander;
+use zea_parser::{ModuleParser, NodeIdGenerator};
+
 fn main() {
-    // let src = read_to_string("zea-driver/test.zea").unwrap();
-    // let c = grammar::ExprParser::new();
-    // let module = parse(&src);
-    // let module = match module {
-    //     Ok((module, errs)) => {
-    //         for err in errs {
-    //             eprintln!("{err}\n++++++++++++++++++++++++++++++++++++++++++++++++");
-    //         }
-    //         module },
-    //     Err(e) => {
-    //         eprintln!("{e}");
-    //         exit(1)
-    //     }
-    // };
-    // eprint!("{module:?}");
+    let src = read_to_string("zea-driver/test.zea").unwrap();
+    let mut generator = NodeIdGenerator::new();
+    let p = ModuleParser::new();
+    let module = p.parse(&mut generator, &src);
+    let module = match module {
+        Ok(module) => {
+            println!("parsed {}", module.pretty_print(0));
+            module
+        }
+        Err(e) => {
+            eprintln!("{e}");
+            exit(1)
+        }
+    };
+    
+    let mut node_expander = NodeExpander::new();
+    let module = module.expand_blocks(&mut node_expander);
+    let module = module.expand_blocks(&mut node_expander);
+    
+    
 }
