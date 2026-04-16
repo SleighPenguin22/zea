8c8bd49a0799a70a726fec22e770a1d64397b3c9
Iwan Snel <sneliwan40@gmail.com>
2026-04-16 22:35:06 +0200

    warnings

--- zea-ast/src/zea/mod.rs
+++ zea-ast/src/zea/mod.rs
@@ -607,7 +607,7 @@ pub enum AssignmentPattern {
 impl StructuralEq for AssignmentPattern {
     fn eq_ignore_id(&self, other: &Self) -> bool {
         match (self, other) {
-            (AssignmentPattern::Identifier(s1), AssignmentPattern::Identifier(s2)) => true,
+            (AssignmentPattern::Identifier(_), AssignmentPattern::Identifier(_)) => true,
             (AssignmentPattern::Tuple(t1), AssignmentPattern::Tuple(t2)) => {
                 t1.iter().zip(t2).all(|(a, b)| a.eq_ignore_id(b))
             }
@@ -629,13 +629,13 @@ pub enum MatchPattern {
 impl StructuralEq for MatchPattern {
     fn eq_ignore_id(&self, other: &Self) -> bool {
         match (self, other) {
-            (MatchPattern::Identifier(_), MatchPattern::Identifier(s2)) => true,
+            (MatchPattern::Identifier(_), MatchPattern::Identifier(_)) => true,
             (MatchPattern::Tuple(t1), MatchPattern::Tuple(t2)) => t1
                 .iter()
                 .zip(t2)
                 .all(|(a, b)| StructuralEq::eq_ignore_id(a, b)),
-            (MatchPattern::UnionVariant(_, _, s3), MatchPattern::UnionVariant(o1, o2, o3)) => {
-                (StructuralEq::eq_ignore_id(s3.as_ref(), o3.as_ref()))
+            (MatchPattern::UnionVariant(_, _, s3), MatchPattern::UnionVariant(_, _, o3)) => {
+                StructuralEq::eq_ignore_id(s3.as_ref(), o3.as_ref())
             }
             _ => false,
         }
